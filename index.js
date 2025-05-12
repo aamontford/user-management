bb7c5b22dce22e17c7a62c435c29f30b303b1c0d
aamontford <amamontford1@gmail.com>
2025-05-12 14:34:00 +0000

    Created the structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db'); 
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json()); 
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

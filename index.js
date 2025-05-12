b112fb0a9aa481cf53c8a4b817ebea8e8a2b48fa
aamontford <amamontford1@gmail.com>
2025-05-12 16:52:26 +0000

    worked on user login and registration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db'); 
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 connectDB();
@@ -9,6 +10,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); 
+app.use('/api', authRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ routes/authRoutes.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const router = express.Router();
+
+// Register user
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    if (!name || !email || !password || !role) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // Check if user already exists in the database
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10); 
+    const hashedPassword = await bcrypt.hash(password, salt); 
+
+    const newUser = new User({ name, email, password: hashedPassword, role });
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    console.error("Error during registration:", err.message);
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+});
+
+//Login user
+router.post("/login", async (req,res) => {
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+           return res.status(400).json({ message: "Invalid credentials" });
+    }
+    
+    // Compare entered password with stored hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const payload = { userId: user._id, role: user.role, };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '15m' });
+    res.status(200).json({ message: "User login successful", token: token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
+module.exports = router;
